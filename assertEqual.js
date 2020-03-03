b1dc05dc244f73e1a13412b710663584bff292c3
Sara Cullen <hello.saracullen@gmail.com>
2020-03-03 01:17:04 +0000

    Create assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍 Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("👎 Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 99);
+assertEqual("I need coffee!", "I need coffee!");
